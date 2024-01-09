358402455c93b2d6e162abfb44b4ae62f3132f8d
JashonMenya <jjmenya14@gmail.com>
2024-01-09 11:37:25 -0600

    repositories exercise

---
+++ repositories/src/test/java/com/jmenya/repositories/RepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmenya.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
